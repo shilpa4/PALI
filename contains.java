0739ad27af2608a06a58e7ffa97cbe436cc97b0a
shilpa4 <shilpasre91@gmail.com>
2016-07-29 18:20:28 +0530

    Add files via upload

---
+++ contains.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class contains {
+	public static void main(String arg[]){
+		String s1;
+		Scanner ff=new Scanner(System.in);
+		s1=ff.next();
+		boolean c;
+		if(c=s1.contains(" ")){
+	System.out.println("true");
+		}
+		else{
+		System.out.println("false");
+	}
+	}
+}
